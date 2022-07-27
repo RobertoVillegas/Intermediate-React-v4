a84bd6702ee68776d521f6c36982b6d83a7501e8
Roberto Villegas <hola@robertovillegas.com>
2022-07-27 00:22:46 -0500

    Server Side Rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { render } from "react-dom";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { StrictMode, useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import ThemeContext from "./ThemeContext";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
@@ -11,18 +10,16 @@ const App = () => {
   return (
     <StrictMode>
       <ThemeContext.Provider value={theme}>
-        <BrowserRouter>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
       </ThemeContext.Provider>
     </StrictMode>
   );
 };
 
-render(<App />, document.getElementById("root"));
+export default App;
